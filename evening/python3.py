c23b037ad7f7991414cdaf50e2226cf524b69990
Bharath chengappa <devops@gmail.com>
2025-02-24 20:36:26 +0530

    .

---
+++ evening/python3.py
@@ -0,0 +1,13 @@
+# Python program to multiply two numbers
+
+# Input from user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Multiplying two numbers
+multiplication_result = num1 * num2
+
+# Display the result
+print("The product of", num1, "and", num2, "is:", multiplication_result)
+:wq
+
