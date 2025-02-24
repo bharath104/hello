7df5f8c937a70643919c10970b8ffc1bad261006
Bharath chengappa <devops@gmail.com>
2025-02-24 20:17:02 +0530

    second commit to the python.py

--- evening/python.py
+++ evening/python.py
@@ -10,3 +10,16 @@ sum_result = num1 + num2
 # Display the result
 print("The sum of", num1, "and", num2, "is:", sum_result)
 
+
+# Python program to subtract two numbers
+
+# Input from user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Subtracting two numbers
+subtraction_result = num1 - num2
+
+# Display the result
+print("The difference between", num1, "and", num2, "is:", subtraction_result)
+
